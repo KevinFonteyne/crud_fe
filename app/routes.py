9a9e2ac1b2436f712aa050384cb5cde2b5a655d2
Kevin Fonteyne <kfonteyne@outlook.com>
2022-09-20 14:37:48 -0400

    assignment 3

---
+++ app/routes.py
@@ -0,0 +1,48 @@
+from flask import( 
+    Flask,
+    render_template,
+    request
+)
+import requests
+
+
+app = Flask(__name__)
+
+BACKEND_URL = "http://127.0.0.1:5000/data_types"
+
+
+@app.get("/")
+def get_index():
+    response = requests.get(BACKEND_URL)
+    scan_data = response.json().get("data_types")
+    return render_template("main.html", data_types=scan_data)
+
+
+@app.get("/floats")
+def get_floats_page():
+    url = "%s/%s" % (BACKEND_URL, "Floating")
+    response = requests.get(url)
+    float_data = response.json().get("data_type")
+    return render_template("data_type.html", data_type=float_data[0])
+
+
+@app.get("/create/data_types")
+def get_data_type_from():
+    return render_template("new.html")
+
+
+@app.post("/create/data_types")
+def create_data_type():
+    from_data = request.form
+    new_dt = {
+        "name": form_data.get("name"),
+        "summary": form_data.get("summary"),
+        "description": form_data.get("description")
+    }
+    response = requests.post(BACKEND_URL, json=new_dt)
+    if response.status_code == 204:
+        return render_template("new_succes.html")
+    else:
+        return render_template("failed.html")    
+
+
